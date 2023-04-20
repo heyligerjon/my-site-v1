bda0282b7242c23612b9f53c86ed304e3062bb58
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-04-20 17:33:57 -0400

    updated about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,8 +8,11 @@ export default function About() {
                 <div className='about-img' style={{width: '32.5%', height: 0, paddingBottom: '32.5%'}}>
                     <Image src='/static/img/j-isaacs-headshot2.jpg' alt='J. Isaacs headshot' width={350} height={350}/>
                 </div>
-                <div className='about-detail'>
-                    <p>This is the About page of my personal website.</p>
+                <div className='about-detail' style={{border: '2px solid white', borderRadius: '50px', padding: '25px', height: 'fit-content', maxWidth: '50%'}}>
+                    <p>
+                        This is the About page of my personal website.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum      
+                    </p>
                 </div>
             </div>
         </div>

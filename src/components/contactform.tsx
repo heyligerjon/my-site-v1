09c481f1e70ca20b2c03c1d32adebd2a9016cf2d
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-04-20 16:07:28 -0400

    updated layout

--- src/components/contactform.tsx
+++ src/components/contactform.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import * as Form from '@radix-ui/react-form';
 
 interface ContactFormProps {
   onSubmit: () => void;
@@ -41,32 +42,71 @@ const ContactForm: React.FC<ContactFormProps> = ({ onSubmit }) => {
   return (
     <div id='contact' className='contact-container'>
         <h1 id='contact-header' className='section-header'>Contact</h1>
-        <form id='contact-form' className='contact-form' onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <label htmlFor="email">Email</label>
-            <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="message">Message</label>
-            <textarea
-                id="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
+        <Form.Root id='contact-form' className='FormRoot contact-form' onSubmit={handleSubmit}>
+            <Form.Field className='FormField' name='name'>
+                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className='FormLabel' htmlFor="name">Name</Form.Label>
+                    <Form.Message className="FormMessage" match="valueMissing">
+                        Please enter your name
+                    </Form.Message>
+                </div>
+                <Form.Control asChild>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </Form.Control>
+            </Form.Field>
+
+            <Form.Field className='FormField' name='email'>
+                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className='FormLabel' htmlFor="email">Email</Form.Label>
+                    <Form.Message className="FormMessage" match="valueMissing">
+                        Please enter your email
+                    </Form.Message>
+                    <Form.Message className="FormMessage" match="typeMismatch">
+                        Please provide a valid email
+                    </Form.Message>
+                </div>
+                    <Form.Control asChild>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </Form.Control>
+            </Form.Field>
+
+            <Form.Field className='FormField' name='message'>
+                <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                    <Form.Label className='FormLabel' htmlFor="message">Message</Form.Label>
+                    <Form.Message className="FormMessage" match="valueMissing">
+                        Please enter a message
+                    </Form.Message>
+                </div>
+                    <Form.Control asChild>
+                        <textarea
+                            id="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            required
+                            placeholder='Hi!'
+                        />
+                    </Form.Control>
+            </Form.Field>
             {error && <p>{error}</p>}
-            <button type="submit" disabled={isLoading}>
-                {isLoading ? 'Submitting...' : 'Submit'}
-            </button>
-        </form>
+
+            <Form.Submit asChild>
+                <button className='submit-btn' type="submit" disabled={isLoading}>
+                    {isLoading ? 'Submitting...' : 'Submit'}
+                </button>
+            </Form.Submit>
+        </Form.Root>
     </div>
   );
 };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,11 +4,13 @@ export default function About() {
     return (
         <div id='about' className='about-container'>
             <h1 id='about-header' className='section-header'>About Me</h1>
-            <div className='about-img'>
-                <Image src='' alt='J. Isaacs headshot'/>
-            </div>
-            <div className='about-detail'>
-                <p>This is the About page of my personal website.</p>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <div className='about-img' style={{width: '32.5%', height: 0, paddingBottom: '32.5%'}}>
+                    <Image src='/static/img/j-isaacs-headshot2.jpg' alt='J. Isaacs headshot' width={350} height={350}/>
+                </div>
+                <div className='about-detail'>
+                    <p>This is the About page of my personal website.</p>
+                </div>
             </div>
         </div>
     );

1777a549185a6cdf292d22dda40f6b5ad4302926
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-04-21 17:09:46 -0400

    added styling and tab widget for experience page

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -6,30 +6,38 @@ export default function Experience() {
             <h1 id='exp-header' className='section-header'>Experience</h1>
             <div className='exp-tabs'>
                 <Tabs.Root className='TabsRoot' defaultValue='Tab1' orientation="vertical" style={{display: 'flex'}}>
-                    <Tabs.List className='TabsList' aria-label='Past Experience' style={{display: 'flex', flexDirection: 'column'}}>
-                        <Tabs.Trigger className='TabsTrigger' value="Tab1" style={{border: '1px solid white'}}>
+                    <Tabs.List className='TabsList tab-list' aria-label='Past Experience' style={{display: 'flex', flexDirection: 'column'}}>
+                        <Tabs.Trigger className='TabsTrigger tab tab-1' value="Tab1">
                             Founder of crate.digital
                         </Tabs.Trigger>
-                        <Tabs.Trigger className='TabsTrigger' value="Tab2" style={{border: '1px solid white'}}>
+                        <Tabs.Trigger className='TabsTrigger tab tab-2' value="Tab2">
                             Freelance Developer
                         </Tabs.Trigger>
-                        <Tabs.Trigger className='TabsTrigger' value="Tab3" style={{border: '1px solid white'}}>
+                        <Tabs.Trigger className='TabsTrigger tab tab-3' value="Tab3">
                             Software Engineer @ FAST Enterprises
                         </Tabs.Trigger>
-                        <Tabs.Trigger className='TabsTrigger' value="Tab4" style={{border: '1px solid white'}}>
+                        <Tabs.Trigger className='TabsTrigger tab tab-4' value="Tab4">
                             University of Georgia
                         </Tabs.Trigger>
                     </Tabs.List>
-                    <Tabs.Content className='TabsContent' value='Tab1' style={{border: '1px solid white'}}>
+                    <Tabs.Content className='TabsContent tab-content' value='Tab1'>
+                        <h5>Mar 2023-Present</h5>
+                        <h6>Detroit, MI</h6>
                         <p>add description 1 here</p>
                     </Tabs.Content>
-                    <Tabs.Content className='TabsContent' value='Tab2' style={{border: '1px solid white'}}>
+                    <Tabs.Content className='TabsContent tab-content' value='Tab2'>
+                        <h5>Dec 2022-Present</h5>
+                        <h6>Detroit, MI</h6>
                         <p>add description 2 here</p>
                     </Tabs.Content>
-                    <Tabs.Content className='TabsContent' value='Tab3' style={{border: '1px solid white'}}>
+                    <Tabs.Content className='TabsContent tab-content' value='Tab3'>
+                        <h5>Aug 2019-Nov 2022</h5>
+                        <h6>St. Paul, MN & Detroit, MI</h6>
                         <p>add description 3 here</p>
                     </Tabs.Content>
-                    <Tabs.Content className='TabsContent' value='Tab4' style={{border: '1px solid white'}}>
+                    <Tabs.Content className='TabsContent tab-content' value='Tab4'>
+                        <h5>Aug 2015-May 2019</h5>
+                        <h6>Athens, GA</h6>
                         <p>add description 4 here</p>
                     </Tabs.Content>
                 </Tabs.Root>

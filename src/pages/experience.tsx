ae0fb781c3943e612e41962aad297c4d64954ee2
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-05-04 17:20:09 -0400

    updated experience page and nav styles

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -4,7 +4,7 @@ export default function Experience() {
     return (
         <div id='experience' className='exp-container'>
             <h1 id='exp-header' className='section-header gradient'>Experience</h1>
-            <div className='exp-tabs'>
+            <div className='exp-tabs featurette'>
                 <Tabs.Root className='TabsRoot' defaultValue='Tab1' orientation="vertical" style={{display: 'flex'}}>
                     <Tabs.List className='TabsList tab-list' aria-label='Past Experience'>
                         <Tabs.Trigger className='TabsTrigger tab tab-1 btn-gradient' value="Tab1">
@@ -14,26 +14,46 @@ export default function Experience() {
                             Freelance Developer
                         </Tabs.Trigger>
                         <Tabs.Trigger className='TabsTrigger tab tab-3 btn-gradient' value="Tab3">
-                            Software Engineer @ FAST Enterprises
+                            Software Engineer
                         </Tabs.Trigger>
                         <Tabs.Trigger className='TabsTrigger tab tab-4 btn-gradient' value="Tab4">
-                            University of Georgia
+                            Implementation Consultant
                         </Tabs.Trigger>
                     </Tabs.List>
                     <Tabs.Content className='TabsContent tab-content' value='Tab1'>
+                        <h4>Founder of crate.digital</h4>
                         <h5>Detroit, MI</h5>
                         <h6>Mar 2023-Present</h6>
                         <p>add description 1 here</p>
                     </Tabs.Content>
                     <Tabs.Content className='TabsContent tab-content' value='Tab2'>
+                        <h4>Freelance Developer</h4>
                         <h5>Detroit, MI</h5>
                         <h6>Dec 2022-Present</h6>
                         <p>add description 2 here</p>
                     </Tabs.Content>
                     <Tabs.Content className='TabsContent tab-content' value='Tab3'>
-                        <h5>St. Paul, MN & Detroit, MI</h5>
-                        <h6>Aug 2019-Nov 2022</h6>
-                        <p>add description 3 here</p>
+                        <h4>Software Engineer</h4>
+                        <h5>Detroit, MI</h5>
+                        <h6>Jan 2021-Nov 2022</h6>
+                        <p className='description'>
+                            As a software engineer, I was fortunate to work on some very challenging projects that taught me a lot about code in a professional setting. Through these experiences, I gained valuable insights into how complex systems are integrated and developed a real passion for delivering innovative solutions that make a difference. Here is a bit of what I accomplished in my time:
+                            <ul className='tab-ul exp-list'>
+                                <li className='exp-list-detail'>
+                                    Created a NASWA interface using a RESTful API to exchange web requests with over 5 million users
+                                </li>
+                                <li className='exp-list-detail'>
+                                    Rebuilt legacy VB.NET code in C#, updating algorithms to increase performance in jobs by up to 70%
+                                </li>
+                            </ul>
+                        </p>
+                        <p className='description'>
+                            <h5>Skills</h5>
+                            <ul className='tab-ul skills-list'>
+                                <li className='skills-list-detail'>C#</li>
+                                <li className='skills-list-detail'>SQL</li>
+                            </ul>
+                        </p>
                     </Tabs.Content>
                     <Tabs.Content className='TabsContent tab-content' value='Tab4'>
                         <h5>Athens, GA</h5>
--- src/pages/featured.tsx
+++ src/pages/featured.tsx
@@ -1,48 +1,51 @@
 import Image from "next/image";
+//import Carousel from '@/components/carousel';
 
 export default function Featured() {
     return (
         <div id='featured' className='feat-container'>
-          <h1 id='feat-header' className='section-header gradient'>Featured Projects</h1>
-          <div className='project-carousel'>
-            <ul className='featured-projects'>
-                <li id='project-1' className='project'>
-                    <h4 className='project-header'>Project 1</h4>
-                    <Image className='project-img' src={""} alt={""} />
-                    <p className='project-description'>
-                        This is a description for my first featured project.
-                    </p>
-                </li>
-                <li id='project-2' className='project'>
-                    <h4 className='project-header'>Project 2</h4>
-                    <Image className='project-img' src={""} alt={""} />
-                    <p className='project-description'>
-                        This is a description for my second featured project.
-                    </p>
-                </li>
-                <li id='project-3' className='project'>
-                    <h4 className='project-header'>Project 3</h4>
-                    <Image className='project-img' src={""} alt={""} />
-                    <p className='project-description'>
-                        This is a description for my third featured project.
-                    </p>
-                </li>
-                <li id='project-4' className='project'>
-                    <h4 className='project-header'>Project 4</h4>
-                    <Image className='project-img' src={""} alt={""} />
-                    <p className='project-description'>
-                        This is a description for my fourth featured project.
-                    </p>
-                </li>
-                <li id='project-5' className='project'>
-                    <h4 className='project-header'>Project 5</h4>
-                    <Image className='project-img' src={""} alt={""} />
-                    <p className='project-description'>
-                        This is a description for my fifth featured project.
-                    </p>
-                </li>
-            </ul>
-          </div>
+            <h1 id='feat-header' className='section-header gradient'>Featured Projects</h1>
+            <div id="featurette" className="featurette">
+                <div className='project-carousel'>
+                    <ul className='featured-projects'>
+                        <li id='project-1' className='project'>
+                            <h4 className='project-header'>Project 1</h4>
+                            <Image className='project-img' src={""} alt={""} />
+                            <p className='project-description'>
+                                This is a description for my first featured project.
+                            </p>
+                        </li>
+                        <li id='project-2' className='project'>
+                            <h4 className='project-header'>Project 2</h4>
+                            <Image className='project-img' src={""} alt={""} />
+                            <p className='project-description'>
+                                This is a description for my second featured project.
+                            </p>
+                        </li>
+                        <li id='project-3' className='project'>
+                            <h4 className='project-header'>Project 3</h4>
+                            <Image className='project-img' src={""} alt={""} />
+                            <p className='project-description'>
+                                This is a description for my third featured project.
+                            </p>
+                        </li>
+                        <li id='project-4' className='project'>
+                            <h4 className='project-header'>Project 4</h4>
+                            <Image className='project-img' src={""} alt={""} />
+                            <p className='project-description'>
+                                This is a description for my fourth featured project.
+                            </p>
+                        </li>
+                        <li id='project-5' className='project'>
+                            <h4 className='project-header'>Project 5</h4>
+                            <Image className='project-img' src={""} alt={""} />
+                            <p className='project-description'>
+                                This is a description for my fifth featured project.
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>  
         </div>
       );
 }

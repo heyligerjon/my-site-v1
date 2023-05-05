4e2a17c5c6089aa4a2cc5554c7e0e7c2ecd3c598
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-05-05 16:58:47 -0400

    added custom fonts

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -24,38 +24,38 @@ export default function Experience() {
                         <h4>Freelance Developer</h4>
                         <h5>Detroit, MI</h5>
                         <h6>Dec 2022-Present</h6>
-                        <p className='description'>
+                        <div className='description'>
                             <div className='blurb'>
                                 Following a massive personal upheaval, I found myself craving new direction. One that would allow me to pursue my passion for creative development. Leaving my job was an incredibly difficult decision, but I knew it was the right path. Starting this journey has been a mixture of excitement and uncertainty, as I figure out how to navigate an industry that moves faster than you can blink.
                             <br></br>
                             <br></br>
                                 I have worked tirelessly on a range of projects that have allowed me to expand my skillset and grow as a web developer, and I am determined build a strong reputation for myself that will follow me wherever I go. Each day affords me countless opportunities to learn that I wake up every morning eager to embrace. Though it's still early days, I'm committed to putting in the hard work needed to succeed, and to building a fulfilling career that aligns with my passion for development.
                             </div>
-                        </p>
+                        </div>
                     </Tabs.Content>
                     <Tabs.Content className='TabsContent tab-content' value='Tab2'>
                         <h4>Founder of crate.digital</h4>
                         <h5>Detroit, MI</h5>
                         <h6>Mar 2023-Present</h6>
-                        <p className='description'>
+                        <div className='description'>
                             <div className='blurb'>
                                 Starting a digital media company has been a journey filled with unexpected challenges. From discovering new technologies and learning to navigate an ever-changing industry, to developing a brand identity to creating engaging material, every step has required a steep learning curve. However, each milestone reached has been a great source of pride, and I remain excited about the possibilities that lie ahead. Despite the obstacles, I am determined to succeed and continue growing my skills as an developer.
                             </div>
-                        </p>
-                        <p className='description'>
+                        </div>
+                        <div className='description'>
                             <h5>Skills</h5>
                             <ul className='tab-ul skills-list'>
                                 <li className='skills-list-detail'>Python</li>
                                 <li className='skills-list-detail'>React.js</li>
                                 <li className='skills-list-detail'>HTML/CSS</li>
                             </ul>
-                        </p>
+                        </div>
                     </Tabs.Content>
                     <Tabs.Content className='TabsContent tab-content' value='Tab3'>
                         <h4>Software Engineer</h4>
                         <h5>Detroit, MI</h5>
                         <h6>Jan 2021-Nov 2022</h6>
-                        <p className='description'>
+                        <div className='description'>
                             <div className='blurb'>
                                 As a software engineer, I was fortunate to work on some very challenging projects that taught me a lot about code in a professional setting. Through these experiences, I gained valuable insights into how complex systems are integrated and developed a real passion for delivering innovative solutions that make a difference. Here is a bit of what I accomplished in my time:
                             </div>
@@ -67,21 +67,21 @@ export default function Experience() {
                                     Rebuilt legacy VB.NET code in C#, updating algorithms to increase performance in jobs by up to 70%
                                 </li>
                             </ul>
-                        </p>
-                        <p className='description'>
+                        </div>
+                        <div className='description'>
                             <h5>Skills</h5>
                             <ul className='tab-ul skills-list'>
                                 <li className='skills-list-detail'>C#</li>
                                 <li className='skills-list-detail'>.NET 6</li>
                                 <li className='skills-list-detail'>DB Management</li>
                             </ul>
-                        </p>
+                        </div>
                     </Tabs.Content>
                     <Tabs.Content className='TabsContent tab-content' value='Tab4'>
                         <h4>Implementation Consultant</h4>
                         <h5>St. Paul, MN</h5>
                         <h6>Aug 2015-May 2019</h6>
-                        <p className='description'>
+                        <div className='description'>
                             <div className='blurb'>
                                 As a consultant, I found great fulfillment in helping individuals in the organization find solutions that fit what they needed. From implementing new software to troubleshooting hardware issues, I enjoyed providing results that mattered. While the work was rewarding, I also desired a more technical challenge. Looking back, I'm very grateful for the professional experience and the opportunity to grow my skills in that area.
                             </div>
@@ -93,15 +93,15 @@ export default function Experience() {
                                     Assisted with integration and deployment of full codebase conversion, along with security, database, and other tools for a team of 50+ developers
                                 </li>
                             </ul>
-                        </p>
-                        <p className='description'>
+                        </div>
+                        <div className='description'>
                             <h5>Skills</h5>
                             <ul className='tab-ul skills-list'>
                                 <li className='skills-list-detail'>SQL</li>
                                 <li className='skills-list-detail'>Powershell</li>
                                 <li className='skills-list-detail'>Visual Basic</li>
                             </ul>
-                        </p>
+                        </div>
                     </Tabs.Content>
                 </Tabs.Root>
             </div>

54e586cbe6f0cfda0fb3342a59f6ee611246a251
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-05-08 15:56:25 -0400

    added featured page styles and updated components

--- src/pages/featured.tsx
+++ src/pages/featured.tsx
@@ -8,40 +8,45 @@ export default function Featured() {
             <div id="featurette" className="featurette">
                 <div className='project-carousel'>
                     <ul className='featured-projects'>
-                        <li id='project-1' className='project'>
+                        <li id='project-1' className='project-div'>
                             <h4 className='project-header'>Project 1</h4>
-                            <Image className='project-img' src={""} alt={""} />
-                            <p className='project-description'>
-                                This is a description for my first featured project.
-                            </p>
+                            <div className="card">
+                                <p className='project-description'>
+                                    <code>crate.digital</code> is an upcoming blog and digital media site focused on creating a space to promote underground musicians and artists organically. Like digging through a digital record crate.
+                                </p>
+                            </div>
                         </li>
-                        <li id='project-2' className='project'>
+                        <li id='project-2' className='project-div'>
                             <h4 className='project-header'>Project 2</h4>
-                            <Image className='project-img' src={""} alt={""} />
-                            <p className='project-description'>
-                                This is a description for my second featured project.
-                            </p>
+                            <div className="card">
+                                <p className='project-description'>
+                                    A simple social media web app that allows you to update statuses and communicate with users 
+                                </p>
+                            </div>
                         </li>
-                        <li id='project-3' className='project'>
+                        <li id='project-3' className='project-div'>
                             <h4 className='project-header'>Project 3</h4>
-                            <Image className='project-img' src={""} alt={""} />
-                            <p className='project-description'>
-                                This is a description for my third featured project.
-                            </p>
+                            <div className="card">
+                                <p className='project-description'>
+                                    This is a description for my third featured project.
+                                </p>
+                            </div>
                         </li>
-                        <li id='project-4' className='project'>
+                        <li id='project-4' className='project-div'>
                             <h4 className='project-header'>Project 4</h4>
-                            <Image className='project-img' src={""} alt={""} />
-                            <p className='project-description'>
-                                This is a description for my fourth featured project.
-                            </p>
+                            <div className="card">
+                                <p className='project-description'>
+                                    This is a description for my fourth featured project.
+                                </p>
+                            </div>
                         </li>
-                        <li id='project-5' className='project'>
+                        <li id='project-5' className='project-div'>
                             <h4 className='project-header'>Project 5</h4>
-                            <Image className='project-img' src={""} alt={""} />
-                            <p className='project-description'>
-                                This is a description for my fifth featured project.
-                            </p>
+                            <div className="card">
+                                <p className='project-description'>
+                                    This is a description for my fifth featured project.
+                                </p>
+                            </div>
                         </li>
                     </ul>
                 </div>

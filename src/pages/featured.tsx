59c5f23926e0b72e5e4ab7c50eabbf06fc9985ac
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-05-09 14:22:18 -0400

    updated featured projects

--- src/pages/featured.tsx
+++ src/pages/featured.tsx
@@ -9,42 +9,42 @@ export default function Featured() {
                 <div className='project-carousel'>
                     <ul className='featured-projects'>
                         <li id='project-1' className='project-div'>
-                            <h4 className='project-header'>Project 1</h4>
+                            <h4 className='project-header'><code>crate.digital</code></h4>
                             <div className="card">
                                 <p className='project-description'>
                                     <code>crate.digital</code> is an upcoming blog and digital media site focused on creating a space to promote underground musicians and artists organically. Like digging through a digital record crate.
                                 </p>
                             </div>
                         </li>
-                        <li id='project-2' className='project-div'>
+                        {/* <li id='project-2' className='project-div'>
                             <h4 className='project-header'>Project 2</h4>
                             <div className="card">
                                 <p className='project-description'>
                                     A simple social media web app that allows you to update statuses and communicate with users 
                                 </p>
                             </div>
-                        </li>
+                        </li> */}
                         <li id='project-3' className='project-div'>
-                            <h4 className='project-header'>Project 3</h4>
+                            <h4 className='project-header'>Social Network</h4>
                             <div className="card">
                                 <p className='project-description'>
-                                    This is a description for my third featured project.
+                                A simple social media web app that allows you to update statuses and communicate with users 
                                 </p>
                             </div>
                         </li>
                         <li id='project-4' className='project-div'>
-                            <h4 className='project-header'>Project 4</h4>
+                            <h4 className='project-header'>Wiki</h4>
                             <div className="card">
                                 <p className='project-description'>
-                                    This is a description for my fourth featured project.
+                                    A Wikipedia-like online encyclopedia, where entries can be viewed by visiting each entry's page.
                                 </p>
                             </div>
                         </li>
                         <li id='project-5' className='project-div'>
-                            <h4 className='project-header'>Project 5</h4>
+                            <h4 className='project-header'>React ToDo</h4>
                             <div className="card">
                                 <p className='project-description'>
-                                    This is a description for my fifth featured project.
+                                    Single-page web app using React.js that allows users to create and edit a to-do list.
                                 </p>
                             </div>
                         </li>

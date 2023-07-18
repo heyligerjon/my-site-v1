ebca7daa510addc6809ee5f9ba6e5f4edf9f8fe5
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-07-18 18:21:03 -0400

    hide footer on exp pg

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -19,15 +19,19 @@ function useWindowSize() {
 
 export default function Experience() {
     const [width, height] = useWindowSize();
+    // need to move logic to footer element to remove DOM error
     var nav = document.getElementById("navbar");
+    var footer = document.getElementById('footer');
 
-    if (nav) {
-        nav.style.display = 'none';
+    if (nav && footer) {
+        nav.style.display = 'none'
+        footer.style.display = 'none'
     }
 
     return (
-        <div id='experience' className='container flex justify-center self-center'>
-            <Document className='' file='/static/files/react-resume.pdf'>
+        <div id='experience' className='container absolute flex justify-center self-center'>
+            <button className='relative text-3xl self-start -left-20 btn-gradient border-2 border-dark p-3 h-fit mt-8' onClick={() => history.back()}>&lt;-</button>
+            <Document className='relative -left-4' file='/static/files/react-resume.pdf'>
                 <Page className='flex justify-end' pageNumber={1} width={width/2} renderTextLayer={false} renderAnnotationLayer={false}/>
             </Document>
         </div>

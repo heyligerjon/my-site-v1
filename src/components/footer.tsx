59754c17dfbd0c150709ee89ad5df24907355c02
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-08-10 12:27:55 -0400

    footer margin bug fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return(
-        <footer id='footer' className="flex flex-col justify-center items-center bg-light border-t p-10 mt-auto">
+        <footer id='footer' className="flex flex-col justify-center items-center bg-light border-t p-10">
             <p>&copy; 2023 Crate Digital, LLC.</p>
             <p>All rights reserved.</p>
         </footer>
